ac7bdf5dc7190453136cd9633b573c43f5da6e39
Brandon Morelli <bmorelli25@gmail.com>
2017-05-27 11:29:43 -0700

    add code to favorite 10 tweets on subject

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-var twit = require('twit');
+var Twitter = require('twitter');
 var config = require('./config.js');
+var T = new Twitter(config);
+
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+  if(!err){
+    for(let i = 0; i < data.statuses.length; i++){
+      let id = { id: data.statuses[i].id_str }
+      T.post('favorites/create', id, function(err, response){
+        if(err){
+          console.log(err[0].message);
+          //[{code: 139, message: 'You have already favorited this status.'}]
+        }
+        else{
+          let username = response.user.screen_name;
+          let tweetId = response.id_str;
+          console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+        }
+      });
+    }
+  } else {
+    console.log(err);
+  }
+})
---
+++ readme.md
@@ -0,0 +1,13 @@
+**Coming Soon**
+
+
+Note - you need a `config.js` file for this bot to work. The structure should be the following:
+
+```
+module.exports = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+}
+```
